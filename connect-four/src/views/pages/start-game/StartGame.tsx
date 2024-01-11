720387b14bcc854024bb11cb98ccb0881e718ef0
Vladyslav <5vladik068@gmail.com>
2024-01-11 11:22:36 +0200

    Start Game Page

--- connect-four/src/views/pages/start-game/StartGame.tsx
+++ connect-four/src/views/pages/start-game/StartGame.tsx
@@ -1,3 +1,35 @@
+import { Link } from "react-router-dom";
+import { GAME_ROUTE, INFO_ROUTE, DASHBOARD_ROUTE } from "../../../routes";
+
 export function StartGamePage(): JSX.Element {
-  return <h1>Welcome page</h1>;
+  return (
+    <div className="flex-col">
+      <h1 className=" text-yellow text-7xl mb-10 animate-wiggle">
+        Connect Four
+      </h1>
+      <div className=" flex flex-col gap-5 text-center text-xl">
+        <Link
+          to={GAME_ROUTE}
+          className="py-3 mx-16 bg-white text-blue border rounded-lg hover:-translate-y-1 active:translate-y-1.5 duration-200 shadow-lg hover:shadow-xl active:shadow-md"
+        >
+          Start
+        </Link>
+        <Link
+          to={INFO_ROUTE}
+          className="py-3 mx-16 bg-white text-blue border rounded-lg hover:-translate-y-1 active:translate-y-1.5 duration-200 shadow-lg hover:shadow-xl active:shadow-md"
+        >
+          Info
+        </Link>
+        <Link
+          to={DASHBOARD_ROUTE}
+          className="py-3 mx-16 bg-white text-blue border rounded-lg hover:-translate-y-1 active:translate-y-1.5 duration-200 shadow-lg hover:shadow-xl active:shadow-md"
+        >
+          DashBoard
+        </Link>
+        <button className="py-3 mx-16 bg-white text-blue border rounded-lg hover:-translate-y-1 active:translate-y-1.5 duration-200 shadow-lg hover:shadow-xl active:shadow-md">
+          Exit
+        </button>
+      </div>
+    </div>
+  );
 }
--- connect-four/tailwind.config.js
+++ connect-four/tailwind.config.js
@@ -8,6 +8,17 @@ export default {
         blue: "#435585",
         "light-blue": "#818fb4",
         white: "#f5e8c7",
+        yellow: "#FF9800",
+        red: "#B80000",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 2s ease-in-out infinite",
       },
     },
   },
